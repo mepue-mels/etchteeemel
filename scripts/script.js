8ec97676bd88901881d9a07f5f68c5fdac6381ef
MJ Ronduen <rjhronduen@mymail.mapua.edu.ph>
2024-10-13 16:38:05 +0800

    corrected incorrect names for prompt_text

--- scripts/script.js
+++ scripts/script.js
@@ -1,6 +1,6 @@
 const prompt_button = document.getElementById("prompt_btn")
-const prompt_input = document.getElementById("prompt_input")
+const prompt_text = document.getElementById("prompt_text")
 prompt_button.addEventListener('click', function() {
-    const textValue = prompt_input.textContent
+    const textValue = prompt_text.textContent
     console.log(textValue)
 })
